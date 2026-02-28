b7322c923127a39cdecd53d7f48c77b4ce884f74
mraitsam123-rgb <mraitsam123@gmail.com>
2026-03-01 01:42:43 +0500

    Add files via upload

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+const API = import.meta.env.VITE_API_URL || ""
 
 export const authHeaders = (token) => ({ Authorization: `Bearer ${token}` })
 
